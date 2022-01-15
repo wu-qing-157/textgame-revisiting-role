f432a4d0bab53fe477dcafb3bc71513847f8adef
wu-qing-157 <wuqing157@gmail.com>
2022-01-16 00:39:01 +0800

    fix

--- hash_stat.py
+++ hash_stat.py
@@ -5,11 +5,11 @@ from tqdm import tqdm
 
 def stat_state_hash(rom, t='gt_state'):
     if t == 'gt_state':
-        env = JerichoEnv(rom, 0, args=Namespace(nor=False, randr=False, perturb=False, use_gt_state=True, use_gt_room=False, use_nearby=0, output_dir='logs_temp', no_current=False, no_last_look=False))
+        env = JerichoEnv(rom, 0, args=Namespace(nor=False, randr=False, perturb=False, use_gt_state=True, use_gt_room=False, use_nearby_room=0, output_dir='logs_temp', no_current=False, no_last_look=False))
     elif t == 'gt_room':
-        env = JerichoEnv(rom, 0, args=Namespace(nor=False, randr=False, perturb=False, use_gt_state=False, use_gt_room=True, use_nearby=0, output_dir='logs_temp', no_current=False, no_last_look=False))
+        env = JerichoEnv(rom, 0, args=Namespace(nor=False, randr=False, perturb=False, use_gt_state=False, use_gt_room=True, use_nearby_room=0, output_dir='logs_temp', no_current=False, no_last_look=False))
     elif t == 'nearby':
-        env = JerichoEnv(rom, 0, args=Namespace(nor=False, randr=False, perturb=False, use_gt_state=True, use_gt_room=False, use_nearby=1, output_dir='logs_temp', no_current=False, no_last_look=False))
+        env = JerichoEnv(rom, 0, args=Namespace(nor=False, randr=False, perturb=False, use_gt_state=True, use_gt_room=False, use_nearby_room=1, output_dir='logs_temp', no_current=False, no_last_look=False))
     else:
         assert False
     walkthrough = env.env.get_walkthrough()
