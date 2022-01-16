b7a7619b54b93193eb59618bada81fcfaf7d1795
wu-qing-157 <wuqing157@gmail.com>
2022-01-16 09:22:23 +0800

    update

--- hash_stat.py
+++ hash_stat.py
@@ -5,7 +5,7 @@ from argparse import Namespace
 from tqdm import tqdm
 
 
-def stat_state_hash(rom, t='gt_state', log_same=False):
+def stat_state_hash(rom, traj, t='gt_state', log_same=False):
     if t == 'gt_state':
         env = JerichoEnv(rom, 0, get_valid=False, args=Namespace(nor=False, randr=False, perturb=False, use_gt_state=True, use_gt_room=False, use_nearby_room=0, output_dir='logs_temp', no_current=False, no_last_look=False))
     elif t == 'gt_room':
@@ -14,36 +14,37 @@ def stat_state_hash(rom, t='gt_state', log_same=False):
         env = JerichoEnv(rom, 0, get_valid=False, args=Namespace(nor=False, randr=False, perturb=False, use_gt_state=False, use_gt_room=False, use_nearby_room=1, output_dir='logs_temp', no_current=False, no_last_look=False))
     else:
         env = JerichoEnv(rom, 0, get_valid=False, args=Namespace(nor=False, randr=False, perturb=False, use_gt_state=False, use_gt_room=False, use_nearby_room=0, output_dir='logs_temp', no_current=False, no_last_look=False))
-    walkthrough = env.env.get_walkthrough()
-    ret = []
-    obs, info = env.reset()
-    if t == 'drrn_hash':
-        ret.append((hash((info['look'], info['inv']))))
-    else:
-        ret.append(hash(info['state_hash']))
-    count = 0
-    for action in tqdm(walkthrough):
-        obs, _, done, info = env.step(action)
+    # walkthrough = env.env.get_walkthrough()
+    ret = set()
+
+    def update(obs_, info_):
         if t == 'drrn_hash':
-            ret.append((hash((info['look'], info['inv']))))
+            ret.add((hash((obs_, info_['look'], info_['inv']))))
         else:
-            ret.append(hash(info['state_hash']))
-        if ret[-1] != ret[-2]:
-            count += 1
-        elif log_same:
-            print(action)
-            print(repr(obs.strip()))
-            # print(info['look'].strip())
-        if done:
-            break
-    print(env.env.get_score())
-    print(t, len(ret) - 1, count)
+            ret.add(hash(info_['state_hash']))
+
+    obs, info = env.reset()
+    update(obs, info)
+    for line in tqdm(open(traj).readlines()):
+        if line.startswith('>> Action'):
+            action = line.split(':')[1][1:]
+            obs, _, _, info = env.step(action)
+            update(obs, info)
+        elif line.startswith('Starting evaluation episode'):
+            obs, info = env.reset()
+            update(obs, info)
+        elif line.startswith('Action'):
+            action = line.split(':')[1][1:].split(',')[0]
+            obs, _, _, info = env.step(action)
+            update(obs, info)
+    print(t, len(ret))
 
 
 if __name__ == '__main__':
     rom = sys.argv[1]
-    stat_state_hash(rom, t='gt_state', log_same=True)
-    stat_state_hash(rom, t='gt_room', log_same=True)
-    stat_state_hash(rom, t='nearby')
-    stat_state_hash(rom, t='room_name')
-    stat_state_hash(rom, t='drrn')
+    traj = sys.argv[2]
+    stat_state_hash(rom, traj, t='gt_state', log_same=True)
+    stat_state_hash(rom, traj, t='gt_room', log_same=True)
+    stat_state_hash(rom, traj, t='nearby')
+    stat_state_hash(rom, traj, t='room_name')
+    stat_state_hash(rom, traj, t='drrn')
